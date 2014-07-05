cernekee/EasyToken

commit cf2c1c33960ab5eb099337e2191d44139e2cd8dd

MainActivity: Don't draw divider if only frag_0 is present
It was observed that on screen rotation, the frag_0/frag_1 divider
suddenly reappeared - even if we were using TokencodeFragment.  So we
will need to cache the visibility status and set it up if the activity
is ever re-created.

Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/easytoken/MainActivity.java
@@ -31,13 +31,20 @@ public class MainActivity extends Activity
 	public static final String TAG = "EasyToken";
 
 	private static final String EXTRA_LAST_MODIFIED = "app.easytoken.last_modified";
+	private static final String EXTRA_TOKEN_PRESENT = "app.easytoken.token_present";
 
 	private long mLastModified;
+	private boolean mTokenPresent;
+
+	private void updateVisibility() {
+		int vis = mTokenPresent ? View.GONE : View.VISIBLE;
+
+		findViewById(R.id.divider).setVisibility(vis);
+		findViewById(R.id.frag_1).setVisibility(vis);
+	}
 
 	private void setupFragment() {
 		Fragment frag;
-		View divider = findViewById(R.id.divider);
-		View frag_1 = findViewById(R.id.frag_1);
 
 		mLastModified = TokenInfo.lastModified;
 		TokenInfo info = TokenInfo.getDefaultToken();
@@ -48,16 +55,17 @@ public class MainActivity extends Activity
 			frag = new TokencodeFragment();
 			frag.setArguments(args);
 
-			divider.setVisibility(View.GONE);
-			frag_1.setVisibility(View.GONE);
+			mTokenPresent = true;
+			updateVisibility();
 		} else {
 			frag = new GettingStartedFragment();
 
 			getFragmentManager().beginTransaction()
 				.replace(R.id.frag_1, new DevidFragment())
 				.commit();
-			divider.setVisibility(View.VISIBLE);
-			frag_1.setVisibility(View.VISIBLE);
+
+			mTokenPresent = true;
+			updateVisibility();
 		}
 
 		getFragmentManager().beginTransaction()
@@ -74,6 +82,8 @@ public class MainActivity extends Activity
 			setupFragment();
 		} else {
 			mLastModified = b.getLong(EXTRA_LAST_MODIFIED);
+			mTokenPresent = b.getBoolean(EXTRA_TOKEN_PRESENT);
+			updateVisibility();
 		}
 	}
 
@@ -81,6 +91,7 @@ public class MainActivity extends Activity
 	protected void onSaveInstanceState(Bundle b) {
 		super.onSaveInstanceState(b);
 		b.putLong(EXTRA_LAST_MODIFIED, mLastModified);
+		b.putBoolean(EXTRA_TOKEN_PRESENT, mTokenPresent);
 	}
 
 	@Override

commit e6dcc8838bc6dcd0977ab300472705f5acd2e502

MainActivity: Unbreak DevidFragment for the "token not present" case
Commit cf2c1c3396 (Don't draw divider if only frag_0 is present)
introduced a regression that caused mTokenPresent to be set to true
for both present and not-present cases.

Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

src/app/easytoken/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends Activity
 				.replace(R.id.frag_1, new DevidFragment())
 				.commit();
 
-			mTokenPresent = true;
+			mTokenPresent = false;
 			updateVisibility();
 		}
 